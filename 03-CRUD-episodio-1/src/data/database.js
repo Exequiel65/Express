7743a251d535e6d75cf8b0c88a66293a457884bd
Exequiel65 <marcos97britos@gmail.com>
2022-01-20 15:53:31 -0300

    comienzo episodio I CRUD

---
+++ 03-CRUD-episodio-1/src/data/database.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    products: JSON.parse(fs.readFileSync(path.join(__dirname, "/productsDataBase.json"), "utf-8")), 
+    writeProductsJSON: (dataBase) => {
+        fs.writeFileSync(path.join(__dirname, "../database/productsDataBase.json"), JSON.stringify(dataBase), "utf-8")
+    },
+ 
+ }
